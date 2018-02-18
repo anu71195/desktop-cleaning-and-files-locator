a43e1fd31136e2f92a9a1fa18a7cbd623604db87
anu <anu71195@gmail.com>
2018-02-19 04:37:03 +0530

    some cleaning of code

--- cleaning.py
+++ cleaning.py
@@ -11,8 +11,6 @@ def escape_string(input_string,e_str): #input_string is the string where escape 
 	e_str="\\"+e_str
 	return e_str.join(input_string)
 def remove_escape(input_string):
-	# print("string here")
-	# print(input_string)
 	input_string=input_string.split("\\")
 	return "".join(input_string)
 
@@ -32,27 +30,22 @@ def move_directories(directories,source,location):#move directories to the folde
 	os.chdir(location);
 	location=location+"/directories"#location updated to the directory where directories from desktop are to be moved
 	if os.path.exists(location):
-		print("yes exist")
+		pass;
 	else :
 		os.system("mkdir directories")
 		print("directories directory created")
 	os.chdir(source)
 	for dir_name in directories:
-		print(dir_name)
 		move_directory_command="mv -v "+dir_name+" "+location+"/"
 		counter=0;
-		print(location+"/"+dir_name)
 		while(os.path.exists(remove_escape(location)+"/"+remove_escape(dir_name))):
-			print("exist")
 			dir_name=dir_name+str(counter);
 			counter+=1;
 		move_directory_command=move_directory_command+dir_name	
-		print(move_directory_command)
 		os.system(move_directory_command)
 
 def find_extensions(files):
 	extensions=[]
-	print(files)
 	for file_name in files:
 		file_name=file_name.split('.');
 		if(len(file_name)>1):
@@ -69,11 +62,8 @@ def create_all_folders(extensions,source,Documents_location):
 
 def move_files(files,source,location):
 	os.chdir(source)
-	print("\n\n\n\n\n")
 	for file_name in files:
 		# if(len(file_name.split('.'))>1):
-		print(location)
-		print(file_name)
 		if(len(file_name.split('.')[-1])>1):
 			move_file_command="mv -v "+file_name+" "+location+"/"+file_name.split('.')[-1]+"/"
 			file_finaladdress=(remove_escape(location))+"/"+remove_escape(file_name.split('.')[-1])+"/"
@@ -81,20 +71,13 @@ def move_files(files,source,location):
 		else:
 			move_file_command="mv -v "+file_name+" "+location+"/"+"unknown_type"+"/"
 			file_finaladdress=(remove_escape(location))+"/"+"unknown_type/"
-		print(move_file_command)
 		counter=0;
-		print("loc")
-		print(file_finaladdress)
-		print("her")
 		while(os.path.exists(file_finaladdress+remove_escape(file_name.split('.')[0])+str(counter)+"."+remove_escape(file_name.split('.')[1]))):
-			print("existssssssssssssssssssssssssss")
-			# file_name=
 			counter+=1;
 		move_file_command+=file_name.split('.')[0]+str(counter)+"."+file_name.split('.')[1]
 
 		print(move_file_command)
 		os.system(move_file_command)
-	# print(files)
 
 
 
@@ -106,7 +89,6 @@ def move_files(files,source,location):
 Desktop_location=escape_string(os.popen("cd && cd Desktop && pwd").read()[0:-1]," ");#by default os.popen return string ending with \n to remove \n 0:-1 is taken and spaces are escaped in it
 Documents_location=escape_string(os.popen("cd && cd Documents && pwd").read()[0:-1]," ");
 current_location=escape_string(os.popen("pwd").read()[0:-1]," ");
-	# print("here")
 #error handling if there is no Desktop Directory
 try :
 	os.chdir(Desktop_location)
@@ -126,8 +108,6 @@ move_directories(directories,Desktop_location,Documents_location);
 extensions=find_extensions(files);
 create_all_folders(extensions,Desktop_location,Documents_location)
 move_files(files,Desktop_location,Documents_location)
-print(extensions)
-# move_files(files,Desktop_location,Documents_location);
 
 
 
