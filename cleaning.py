7d5d98a367959e59a81181db972ea08e58d96f12
anu <anu71195@gmail.com>
2018-02-19 05:34:46 +0530

    directories are moving properly

--- cleaning.py
+++ cleaning.py
@@ -44,14 +44,14 @@ def move_directories(directories,source,location):#move directories to the folde
 		os.system("mkdir directories")
 		print("directories directory created")
 	os.chdir(source)
-	for dir_name in directories:
-		move_directory_command="mv -v "+dir_name+" "+location+"/"
-		counter=0;
-		while(os.path.exists(remove_escape(location)+"/"+remove_escape(dir_name))):
-			dir_name=dir_name+str(counter);
+	for dir_name in directories:#creating commands to move the directory from desktop to documents
+		move_directory_command="mv -v "+dir_name+" "+location+"/"#verbose move
+		counter=0;#counter to name the duplicate files
+		while(os.path.exists(remove_escape(location)+"/"+remove_escape(dir_name+str(counter)))):#handling the duplicate file names by numbering them
 			counter+=1;
+		dir_name=dir_name+str(counter);	
 		move_directory_command=move_directory_command+dir_name	
-		os.system(move_directory_command)
+		os.system(move_directory_command)#implementing those commands
 
 def find_extensions(files):
 	extensions=[]
@@ -94,34 +94,36 @@ file_names=required_ranged_file(size_type_input[0]);#required output
 print("Required files are as follows:-\n")
 print(file_names)#printing the result for the n largest of n smallest files
 
-
-
-
-
-
-#change afterwards Downloads to Desktop---------------------- note#
-Desktop_location=escape_string(os.popen("cd && cd Desktop && pwd").read()[0:-1]," ");#by default os.popen return string ending with \n to remove \n 0:-1 is taken and spaces are escaped in it
-Documents_location=escape_string(os.popen("cd && cd Documents && pwd").read()[0:-1]," ");
-current_location=escape_string(os.popen("pwd").read()[0:-1]," ");
-#error handling if there is no Desktop Directory
-try :
-	os.chdir(Desktop_location)
+#error handling if there is no Desktop directory
+try:
+	Desktop_location=escape_string(os.popen("cd && cd Desktop && pwd").read()[0:-1]," ");#by default os.popen return string ending with \n to remove \n 0:-1 is taken and spaces as well as parenthesis are escaped in it
 except:
-	print("error no desktop found");
+	print("Error - Desktop is not found in the operating system")
+	print("Exiting...")
 	exit();
 
-#if Documents directory is not present then create it
-if(os.path.isdir(Documents_location)!=True):
-	print("Creating Documents Directory")
-	make_doc_directory_command="cd && mkdir Documents"
-	os.system(make_doc_directory_command)
+#error handling if docments directory is not present then create it
+try:
+	Documents_location=escape_string(os.popen("cd && cd Documents && pwd").read()[0:-1]," ");
+except:
+	print("Documents folder not found")
+	print("Creating Documents folder in home directory")
+	os.system("cd && mkdir Documents")
 
+current_location=escape_string(os.popen("pwd").read()[0:-1]," ");
+os.chdir(Desktop_location)
+print("Finding all the files and folders....",end='\r');
+print("                                                    ",end='\r')
+print("Dividing them into files and folders...",end='\r')
+print("                                                    ",end='\r')
 directories,files=find_dir_files((os.popen("ls -X").read())[0:-1].split("\n"));#list all the files and directories by their type then differentiate between files and directories
-
-move_directories(directories,Desktop_location,Documents_location);
-extensions=find_extensions(files);
-create_all_folders(extensions,Desktop_location,Documents_location)
-move_files(files,Desktop_location,Documents_location)
+print("Moving directories....",end='\r')
+move_directories(directories,Desktop_location,Documents_location);#moves all directories from home to Directories directory in Documents
+print("                                                    ",end='\r')
+print("Finding all the extensions")
+# extensions=find_extensions(files);
+# create_all_folders(extensions,Desktop_location,Documents_location)
+# move_files(files,Desktop_location,Documents_location)
 
 
 
